c3114dfa34d964c8eccf5b6074869a6eb102fec5
William Vidal <williamtv2708@gmail.com>
2017-10-31 21:40:59 -0200

    Validacoes Cadastro_Data_Inicial

--- IndustriaJSF/src/br/upf/topicos/industria/managed/CalendarView.java
+++ IndustriaJSF/src/br/upf/topicos/industria/managed/CalendarView.java
@@ -1,5 +1,7 @@
 package br.upf.topicos.industria.managed;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.faces.application.FacesMessage;
@@ -11,28 +13,66 @@ import org.primefaces.event.SelectEvent;
  
 @ManagedBean
 public class CalendarView {
-	private Date date;
+	public Date date;
+	public Date dateInit;
+	public Date dateEnd;
+	
+	FacesMessage message = null;
+	
+	public DateFormat validaDateFerias() throws ParseException{
+		// RF_03 - Data inicial de férias
+		//verificar a data da escala e setar que nao pode ser menor que ela pra retornar na validação deste campo no cadastro
+		// RF_04 - Data final de férias
+		//verificar a data minDate() e setar que nao pode ser maior que 31 dias pra retornar na validação deste campo no cadastro
+		if(dateInit.after(date)){
+			if(dateInit.before(dateEnd)){
+				// a data da escala é no mínimo a atual
+				// a data inicial é maior que a data da escala
+				// a data final é maior que a data inicial
+				
+				// contador de dias
+				DateFormat df = new SimpleDateFormat ("dd/MM/yyyy");
+				df.setLenient(false);
+				Date d1 = df.parse ("dateInit");
+//				System.out.println (d1);
+				Date d2 = df.parse ("dateEnd");
+//				System.out.println (d2);
+				long dt = (d2.getTime() - d1.getTime()) + 3600000; // 1 hora para compensar horário de verão
+				if(dt / 86400000L < 32){
+					// a diferença entre as datas é menor que 32 dias
+//					System.out.println (dt / 86400000L); // passaram-se 67111 dias
+					return df;
+				}else{
+					message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erro entre as datas de férias", "A diferença ultrapassa 31 dias.");
+				}
+			}else{
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erro na Final Inicial de Férias", "A data final deve ser maior que a data inicial.");
+			}
+		}else{
+			message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erro na Data Inicial de Férias", "A data inicial deve ser maior ou igual que a data da escala.");
+		}
+		return null;
+	}
+	
+	
+	public Date maxDate(){
+		return null;
+	}
+	
+	public Date atualDate(){
+		// RF_06 - Validação data do Cadastro de Funcionário
+		// pegar a data atual aqui
+		Date data = new Date();
+		SimpleDateFormat formatador = new SimpleDateFormat("MM/yyyy");
+		formatador.format( data );
+		return data;
+	}
 	
 	public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat format = new SimpleDateFormat("MM/yyyy");
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Data Selecionada", format.format(event.getObject())));
     }
-     
-	public Date atualDate(){
-		// RF_06 - Validação data do Cadastro de Funcionário
-		// pegar a data atual aqui e retornar
-		Date data = new Date();
-		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
-		formatador.format( data );
-		return data;
-	}
-	
-	public void forMonthDate(){
-		// RF_06 - Validação data do Cadastro de Funcionário
-		// pegar quatro meses a frente da data atual aqui e retornar
-		
-	}
 	
     public void click() {
         RequestContext requestContext = RequestContext.getCurrentInstance();
@@ -48,4 +88,28 @@ public class CalendarView {
     public void setDate(Date date) {
         this.date = date;
     }
+    
+    public Date getDateInit() {
+		return dateInit;
+	}
+
+	public void setDateInit(Date dateInit) {
+		this.dateInit = dateInit;
+	}
+
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+	
+	public FacesMessage getMessage() {
+		return message;
+	}
+	
+	public void setMessage(FacesMessage message) {
+		this.message = message;
+	}
 }
--- IndustriaJSF/src/br/upf/topicos/industria/managed/CalendarViewFerias.java
+++ IndustriaJSF/src/br/upf/topicos/industria/managed/CalendarViewFerias.java
@@ -11,15 +11,25 @@ import org.primefaces.event.SelectEvent;
  
 @ManagedBean
 public class CalendarViewFerias {
-	private Date date;
+	public Date dateInit;
+	public Date dateEnd;
 	
-	public void minDate(){
+	public Date minDate(){
 		// RF_03 - Data inicial de férias
 		//verificar a data da escala e setar que nao pode ser menor que ela pra retornar na validação deste campo no cadastro
 		
 	}
 	
-	public void maxDate(){
+	public Date atualDate(){
+		// RF_03 - Data inicial de férias
+		// pegar a data atual aqui
+		Date data = new Date();
+		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+		formatador.format( data );
+		return data;
+	}
+	
+	public Date maxDate(){
 		// RF_04 - Data final de férias
 		//verificar a data minDate() e setar que nao pode ser maior que 31 dias pra retornar na validação deste campo no cadastro
 		
@@ -28,7 +38,7 @@ public class CalendarViewFerias {
 	public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Data Selecionada", format.format(event.getObject())));
     }
      
     public void click() {
@@ -37,12 +47,20 @@ public class CalendarViewFerias {
         requestContext.update("form:display");
         requestContext.execute("PF('dlg').show()");
     }
-	
-	public Date getDate() {
-        return date;
-    }
- 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+
+	public Date getDateInit() {
+		return dateInit;
+	}
+
+	public void setDateInit(Date dateInit) {
+		this.dateInit = dateInit;
+	}
+
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
 }
