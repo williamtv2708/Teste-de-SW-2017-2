29520603a61fc74e5a95b0149f8f04b0bcd8f234
William Vidal <williamtv2708@gmail.com>
2017-11-01 23:40:08 -0200

    Validacoes Cadastro_Dias

--- IndustriaJSF/src/br/upf/topicos/industria/managed/SelectOneMenuView.java
+++ IndustriaJSF/src/br/upf/topicos/industria/managed/SelectOneMenuView.java
@@ -1,50 +1,48 @@
 package br.upf.topicos.industria.managed;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.model.SelectItem;
-import javax.faces.model.SelectItemGroup;
  
 @ManagedBean
 public class SelectOneMenuView {
      
-    private String console;
+	private String option;  
+    private List<String> options;
    
     @PostConstruct
     public void init() {
-        // itens da selecao
-    	SelectItemGroup g0 = new SelectItemGroup("0");
-    	g0.setSelectItems(new SelectItem[] {new SelectItem("BMW", "BMW")});
-    	
-    	SelectItemGroup g1 = new SelectItemGroup("1");
-    	g1.setSelectItems(new SelectItem[] {new SelectItem("Chrysler", "Chrysler")});
-    	
-    	SelectItemGroup g2 = new SelectItemGroup("2");
-    	g2.setSelectItems(new SelectItem[] {new SelectItem("BMW", "BMW")});
-    	
-    	SelectItemGroup g3 = new SelectItemGroup("3");
-    	g3.setSelectItems(new SelectItem[] {new SelectItem("Chrysler", "Chrysler")});
-    	
-    	SelectItemGroup g4 = new SelectItemGroup("4");
-    	g4.setSelectItems(new SelectItem[] {new SelectItem("BMW", "BMW")});
-    	
-    	SelectItemGroup g5 = new SelectItemGroup("5");
-    	g5.setSelectItems(new SelectItem[] {new SelectItem("Chrysler", "Chrysler")});
-
-    	SelectItemGroup g6 = new SelectItemGroup("6");
-    	g6.setSelectItems(new SelectItem[] {new SelectItem("Chrysler", "Chrysler")});
+    	//options
+        options = new ArrayList<String>();
+        for(int i = 0; i < 7; i++) {
+        	if(i <= 1){
+        		options.add(i + " Dia");
+        	}else{
+        		options.add(i + " Dias");
+        	}
+        }
         
     	// ver como faz pra pegar o valor selecionado
     	
     	// testar o valor selecionado pra mostrar a mensagem de alerta de selecao menor q 3
     	
     }
- 
-    public String getConsole() {
-        return console;
-    }
- 
-    public void setConsole(String console) {
-        this.console = console;
-    }
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+	public List<String> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<String> options) {
+		this.options = options;
+	}
 }
